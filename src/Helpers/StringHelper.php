b91723b6391ca9075b7731ee723f25045bf22b0f
Alex Tartan <alex.tartan@gmail.com>
2018-12-07 01:24:16 +0200

    added pwd generation algo

--- src/Helpers/StringHelper.php
+++ src/Helpers/StringHelper.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace AlexTartan\Helpers;
 
+use InvalidArgumentException;
+
 final class StringHelper
 {
     public static function stripNonPrintableCharacters(string $value): string
@@ -21,5 +23,48 @@ final class StringHelper
         sort($arr);
 
         return implode('', $arr);
+    }
+
+    public static function generateRandomString(
+        int $length,
+        bool $useLower = true,
+        bool $useUpper = true,
+        bool $useDigits = true,
+        bool $useSpecials = true
+    ): string {
+        $alphaLower = 'abcdefghijklmnopqrstuvwxyz';
+        $alphaUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $digits     = '1234567890';
+        $special    = '!@#$%^&*()_+-=[]{}<>';
+
+        $keyspace = str_shuffle(
+            ''
+            . ($useLower ? $alphaLower : '')
+            . ($useUpper ? $alphaUpper : '')
+            . ($useDigits ? $digits : '')
+            . ($useSpecials ? $special : '')
+        );
+
+        $max = mb_strlen($keyspace) - 1;
+        if ($max < 1) {
+            throw new InvalidArgumentException('keyspace must be at least two characters long');
+        }
+        if ($length < count(array_filter([$useLower, $useUpper, $useDigits, $useSpecials]))) {
+            throw new InvalidArgumentException('Length cannot accommodate your requirements. Please increase accordingly.');
+        }
+
+        do {
+            $str = '';
+            for ($i = 0; $i < $length; $i++) {
+                $str .= $keyspace[random_int(0, $max)];
+            }
+            $allDictionariesUsed =
+                ($useLower ? preg_match('/[a-z]/', $str) : true) &&
+                ($useUpper ? preg_match('/[A-Z]/', $str) : true) &&
+                ($useDigits ? preg_match('/[\d]/', $str) : true) &&
+                ($useSpecials ? preg_match('/[[!@#\$%\^&*\(\)_+-=\[\]\{\}<>]]/', $str) : true);
+        } while (!$allDictionariesUsed);
+
+        return $str;
     }
 }
--- test/Helpers/StringHelperTest.php
+++ test/Helpers/StringHelperTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace AlexTartanTest\Helpers;
 
 use AlexTartan\Helpers\StringHelper;
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -24,5 +25,78 @@ final class StringHelperTest extends TestCase
         self::assertEquals('acdg', StringHelper::sortAlphabetically('agdc'));
         self::assertEquals('1acg', StringHelper::sortAlphabetically('g1ac'));
         self::assertEquals(' adz', StringHelper::sortAlphabetically('az d'));
+    }
+
+    public function testRandomStringGeneratorThrowsExceptionOnEmptyKeyspace(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('keyspace must be at least two characters long');
+
+        StringHelper::generateRandomString(16, false, false, false, false);
+    }
+
+    /** @dataProvider lenTooShortDataProvider */
+    public function testRandomStringGeneratorThrowsExceptionOnLengthTooSmall($len): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Length cannot accommodate your requirements. Please increase accordingly.');
+
+        StringHelper::generateRandomString($len);
+    }
+
+    public function lenTooShortDataProvider(): array
+    {
+        return [
+            [-1],
+            [0],
+            [1],
+            [2],
+            [3],
+        ];
+    }
+
+    public function testRandomStringGeneratorLength(): void
+    {
+        for ($i = 16; $i < 32; $i++) {
+            self::assertSame(16, mb_strlen(StringHelper::generateRandomString(16)));
+        }
+    }
+
+    public function testRandomStringGeneratorContainsCorrectKeyspaces(): void
+    {
+        // test contains ONLY lowercase
+        $pass = StringHelper::generateRandomString(16, true, false, false, false);
+        self::assertRegExp('/[a-z]/', $pass);
+        self::assertNotRegExp('/[A-Z]/', $pass);
+        self::assertNotRegExp('/[0-9]/', $pass);
+        self::assertNotRegExp('/[[!@#\$%\^&*\(\)_+-=\[\]\{\}<>]]/', $pass);
+
+        // test contains ONLY uppercase
+        $pass = StringHelper::generateRandomString(16, false, true, false, false);
+        self::assertNotRegExp('/[a-z]/', $pass);
+        self::assertRegExp('/[A-Z]/', $pass);
+        self::assertNotRegExp('/[0-9]/', $pass);
+        self::assertNotRegExp('/[[!@#\$%\^&*\(\)_+-=\[\]\{\}<>]]/', $pass);
+
+        // test contains ONLY numbers
+        $pass = StringHelper::generateRandomString(16, false, false, true, false);
+        self::assertNotRegExp('/[a-z]/', $pass);
+        self::assertNotRegExp('/[A-Z]/', $pass);
+        self::assertRegExp('/[0-9]/', $pass);
+        self::assertNotRegExp('/[[!@#\$%\^&*\(\)_+-=\[\]\{\}<>]]/', $pass);
+
+        // test contains ONLY specials
+        $pass = StringHelper::generateRandomString(16, false, false, false, true);
+        self::assertNotRegExp('/[a-z]/', $pass);
+        self::assertNotRegExp('/[A-Z]/', $pass);
+        self::assertNotRegExp('/[0-9]/', $pass);
+        self::assertRegExp('/[[!@#\$%\^&*\(\)_+-=\[\]\{\}<>]]/', $pass);
+
+        // test contains ALL spaces (by default
+        $pass = StringHelper::generateRandomString(16);
+        self::assertRegExp('/[a-z]/', $pass);
+        self::assertRegExp('/[A-Z]/', $pass);
+        self::assertRegExp('/[0-9]/', $pass);
+        self::assertRegExp('/[[!@#\$%\^&*\(\)_+-=\[\]\{\}<>]]/', $pass);
     }
 }
